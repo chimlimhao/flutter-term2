c03ac3d915cb9fdda85e2cb55d75c8a65c703aa9
limhao2705 <limhao2705@gmail.com>
2025-02-25 23:07:50 +0700

    BLA-000: Start Code

---
+++ lib/screens/ride_pref/ride_pref_screen.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+import '../../model/ride_pref/ride_pref.dart';
+import '../../service/ride_prefs_service.dart';
+import '../../theme/theme.dart';
+import '../../widgets/display/bla_divider.dart';
+
+import 'widgets/ride_pref_form.dart';
+import 'widgets/ride_pref_history_tile.dart';
+
+const String blablaHomeImagePath = 'assets/images/blabla_home.png';
+
+///
+/// This screen allows user to:
+/// - Enter his/her ride preference and launch a search on it
+/// - Or select a last entered ride preferences and launch a search on it
+///
+class RidePrefScreen extends StatefulWidget {
+  const RidePrefScreen({super.key});
+
+  @override
+  State<RidePrefScreen> createState() => _RidePrefScreenState();
+}
+
+class _RidePrefScreenState extends State<RidePrefScreen> {
+  onRidePrefSelected(RidePref ridePref) {
+    // 1 - Navigate to the rides screen (with a buttom to top animation)
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        // 1 - Background Image
+        const BlaBackground(),
+
+        // 2 - Foreground content
+        SafeArea(
+          // Add SafeArea to handle system UI
+          child: SingleChildScrollView(
+            // Make the entire content scrollable
+            child: Column(
+              children: [
+                const SizedBox(height: 16),
+                Text(
+                  "Your pick of rides at low price",
+                  style: BlaTextStyles.heading.copyWith(color: Colors.white),
+                ),
+                const SizedBox(height: 100),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: BlaSpacings.xxl),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      // 2.1 Display the Form to input the ride preferences
+                      RidePrefForm(
+                        initRidePref: RidePrefService.currentRidePref,
+                      ),
+                      const SizedBox(height: BlaSpacings.m),
+
+                      // 2.2 Display past preferences
+                      if (RidePrefService.ridePrefsHistory.isNotEmpty) ...[
+                        const BlaDivider(), // Add visual separator
+                        ListView.builder(
+                          shrinkWrap: true,
+                          physics:
+                              const NeverScrollableScrollPhysics(), // Disable scrolling for inner ListView
+                          itemCount: RidePrefService.ridePrefsHistory.length,
+                          itemBuilder: (ctx, index) => RidePrefHistoryTile(
+                            ridePref: RidePrefService.ridePrefsHistory[index],
+                            onPressed: () => onRidePrefSelected(
+                              RidePrefService.ridePrefsHistory[index],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 16), // Add bottom padding
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class BlaBackground extends StatelessWidget {
+  const BlaBackground({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 340,
+      child: Image.asset(
+        blablaHomeImagePath,
+        fit: BoxFit.cover, // Adjust image fit to cover the container
+      ),
+    );
+  }
+}
